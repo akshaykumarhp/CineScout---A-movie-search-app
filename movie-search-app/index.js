489b72f35a948cee1b29c0805acc9d1f3b6b9f7b
Akshay Kumar Huliyar Prabhakara <akshaykumarhp11@gmail.com>
2024-06-23 14:37:37 -0400

    changed the port

--- movie-search-app/index.js
+++ movie-search-app/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 5000;
+const port = 5002;
 const TMDB_API_KEY = 'cf53dea4dad32e943a22ae8387b2dbaa';
 const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
 
